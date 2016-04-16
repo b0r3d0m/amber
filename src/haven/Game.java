3e9e7f83c885bac0536b1fd76e5f7da605bea72e
Nikita Trophimov <n.trophimov@tools4brokers.com>
2016-04-16 18:47:32 +0400

    Make studyCurio API function place curios to the empty space, not the top-left cell only, add more checks (whether the specified curio is in the study box already)

--- src/haven/Game.java
+++ src/haven/Game.java
@@ -12,19 +12,86 @@ public class Game extends Widget {
             return false;
         }
 
-        List<WItem> charinvwitems = getInventoryWItems(charinvwdg);
-        for (WItem witm : charinvwitems) {
-            String witmbasename = getItemBaseName(witm);
-            if (witmbasename.equals(curioName)) {
-                witm.item.wdgmsg("take", witm.c);
-                // TODO: Drop an item to the empty space, not to the top-left cell
-                // TODO: Handle "not enough study points" situation
-                studyinvwdg.drop(Coord.z, new Coord(0, 0));
-                return true;
+        WItem curiowitm = getInventoryWItem(charinvwdg, curioName);
+        if (curiowitm == null) {
+            return false;
+        }
+
+        WItem studywitem = getInventoryWItem(studyinvwdg, curioName);
+        if (studywitem != null) {
+            return false;
+        }
+
+        List<WItem> studyinvwitems = getInventoryWItems(studyinvwdg);
+        if (studyinvwitems.isEmpty()) {
+
+            curiowitm.item.wdgmsg("take", curiowitm.c);
+            studyinvwdg.drop(Coord.z, new Coord(0, 0));
+            return true;
+
+        }
+
+        Coord invtilesz = new Coord(33, 33);
+        for (int x = 1; x < studyinvwdg.sz.x; x += invtilesz.x) {
+            for (int y = 1; y < studyinvwdg.sz.y; y += invtilesz.y) {
+
+                if (x + curiowitm.sz.x > studyinvwdg.sz.x
+                    || y + curiowitm.sz.y > studyinvwdg.sz.y) {
+                    continue;
+                }
+
+                boolean isIntersect = false;
+                for (WItem witm : studyinvwitems) {
+
+                    Box witmbox = new Box(
+                        new Coord(witm.c.x, witm.c.y),
+                        new Coord(witm.c.x + witm.sz.x, witm.c.y + witm.sz.y)
+                    );
+                    Box desiredbox = new Box(
+                        new Coord(x, y),
+                        new Coord(x + curiowitm.sz.x, y + curiowitm.sz.y)
+                    );
+
+                    if (boxesIntersect(witmbox, desiredbox)) {
+                        isIntersect = true;
+                        break;
+                    }
+
+                }
+
+                if (!isIntersect) {
+                    curiowitm.item.wdgmsg("take", curiowitm.c);
+                    studyinvwdg.drop(Coord.z, new Coord(x, y));
+                    return true;
+                }
+
             }
         }
 
         return false;
+
+    }
+
+    private class Box {
+
+        public Box(Coord topleft, Coord bottomright) {
+            this.topleft = topleft;
+            this.bottomright = bottomright;
+        }
+
+        Coord topleft;
+        Coord bottomright;
+
+    }
+
+    private boolean boxesIntersect(Box lhs, Box rhs) {
+
+        boolean intersect =
+               lhs.topleft.x < rhs.bottomright.x
+            && lhs.bottomright.x > rhs.topleft.x
+            && lhs.topleft.y < rhs.bottomright.y
+            && lhs.bottomright.y > rhs.topleft.y;
+        return intersect;
 
     }
 
@@ -145,6 +212,24 @@ public class Game extends Widget {
         }
 
         return witems;
+
+    }
+
+    private WItem getInventoryWItem(Inventory invwdg, String itemBaseName) {
+
+        for (Widget witm = invwdg.lchild; witm != null; witm = witm.prev) {
+            if (witm instanceof WItem) {
+
+                WItem item = (WItem) witm;
+                String witmbasename = getItemBaseName(item);
+                if (witmbasename.equals(itemBaseName)) {
+                    return item;
+                }
+
+            }
+        }
+
+        return null;
 
     }
 
